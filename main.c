75597289cc8659817cc711cb1f558e9add663f1c
Sayan Chowdhury <user.email : "webber.develo@gmail.com>
2024-12-17 20:28:40 +0530

    gs

--- main.c
+++ main.c
@@ -1,10 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 int main(){
     srand(time(NULL));
     int randomNumber = rand() %100 ;
     printf("%d\n", randomNumber);
-
-
+    int userInput;
+    int numberOfGuesses = 0;
+    printf("!!!  GUESS THE NUMBER  !!!\n");
+    printf("!!!  -RULES:-                    -  !!!\n");
+    printf("!!!  - i) You have to guess      -  !!!\n");
+    printf("!!!  -    a number               -  !!!\n");
+    printf("!!!  - ii) We will return        -  !!!\n");
+    printf("!!!  -     higher or lower based -  !!!\n");
+    printf("!!!  -     on the given number   -  !!!\n");
+do
+{
+    numberOfGuesses++;   
+    printf("!!!  - Guess the number:  ");
+    scanf("%d", &userInput);
+    // printf("-  !!!\n");
+    if (userInput==randomNumber)
+    {
+        printf("CONGRATULATIONS YOU HAVE WON");
+    }
+    else if (userInput > randomNumber)
+    {
+        printf("!!!  - The number is lower       -  !!!\n");
+    }
+    else if (userInput < randomNumber)
+    {
+        printf("!!!  - The number is higher      -  !!!\n");
+    }
+    
+    } while (randomNumber != userInput);
+    
     return 0;
 }
